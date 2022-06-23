113ea11eed0ccad0544a43326c110e8bbe411c16
jberchmas <jberchmas@gmail.com>
2022-06-23 17:19:07 +0200

    Get stage in patient status widget

--- packages/esm-oncology-patient-status-app/src/patient-status-widget.resource.ts
+++ packages/esm-oncology-patient-status-app/src/patient-status-widget.resource.ts
@@ -79,3 +79,59 @@ export function useDiagnosis(patientUuid: string) {
     isValidating,
   };
 }
+
+// Obs
+interface ObsResponse {
+  results: Array<{
+    display: string;
+    voided: boolean;
+    value: {
+      display: string;
+    };
+  }>;
+}
+
+function getObs(patientUuid: string, conceptUuid: string) {
+  const apiUrl = `/ws/rest/v1/obs?patient=${patientUuid}&concept=${conceptUuid}&v=full`;
+  const { data, error, isValidating } = useSWR<{ data: ObsResponse }, Error>(
+    apiUrl,
+    openmrsFetch
+  );
+
+  let obsValue: string | null = "null";
+  const obsSelected = data?.data.results.filter(
+    (observation) => !observation.voided
+  )[0];
+
+  obsValue = obsSelected?.value.display;
+  return {
+    obsValue,
+    isError: error,
+    isLoading: !data && !error,
+    isValidating,
+  };
+}
+
+export function useStage(patientUuid: string) {
+  const obsValue = getObs(patientUuid, "e9cf4aed-34be-4c0a-9004-4294d9bb2d74");
+  const stage = obsValue.obsValue;
+
+  return {
+    stage,
+    isError: obsValue.isError,
+    isLoading: obsValue.isLoading,
+    isValidating: obsValue.isValidating,
+  };
+}
+
+export function useTreatmentPlan(patientUuid: string) {
+  const obsValue = getObs(patientUuid, "3cda0160-26fe-102b-80cb-0017a47871b2");
+  const treatmentPlan = obsValue.obsValue;
+
+  return {
+    treatmentPlan,
+    isError: obsValue.isError,
+    isLoading: obsValue.isLoading,
+    isValidating: obsValue.isValidating,
+  };
+}
--- packages/esm-oncology-patient-status-app/src/patient-status-widget.tsx
+++ packages/esm-oncology-patient-status-app/src/patient-status-widget.tsx
@@ -1,7 +1,11 @@
 import { Tile } from "carbon-components-react";
 import React from "react";
 import { CardHeader } from "./cards";
-import { useDiagnosis } from "./patient-status-widget.resource";
+import {
+  useDiagnosis,
+  useStage,
+  useTreatmentPlan,
+} from "./patient-status-widget.resource";
 import styles from "./patient-status-widget.scss";
 import { ValueTile } from "./value-tile";
 
@@ -12,13 +16,37 @@ interface PatientDashboardWidgetProps {
 function PatientStatusWidget({ patientUuid }: PatientDashboardWidgetProps) {
   const { diagnosis, isError, isLoading, isValidating } =
     useDiagnosis(patientUuid);
+  const { stage } = useStage(patientUuid);
+  const { treatmentPlan } = useTreatmentPlan(patientUuid);
 
   return (
     <div className={styles.widgetCard}>
       <CardHeader title="Patient status">{null}</CardHeader>
-      <Tile light>
-        <ValueTile label="Diagnosis" value={diagnosis ?? "—"} />
-      </Tile>
+
+      <table>
+        <tr>
+          <td>
+            <Tile light>
+              <ValueTile label="Diagnosis" value={diagnosis ?? "—"} />
+            </Tile>
+          </td>
+          <td>
+            <Tile light>
+              <ValueTile label="Stage" value={stage ?? "-"} />
+            </Tile>
+          </td>
+          <td>
+            <Tile light>
+              <ValueTile label="DST Plan" value={treatmentPlan ?? "—"} />
+            </Tile>
+          </td>
+          <td>
+            <Tile light>
+              <ValueTile label="Most recent Obs" value={stage ?? "—"} />
+            </Tile>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
